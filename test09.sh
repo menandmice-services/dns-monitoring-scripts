1e110a3aa649c5eb4adfb4b6df993eca646aa621
Carsten Strotmann <carsten@strotmann.de>
2016-11-02 15:40:06 +0100

    Test 9

---
+++ test09.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Test 9 - test for AA-Flag. Repeat this test for each authoritative
+# server for the zone. Each server must respond with an AA-Flag
+
+dig ns ${1} +short | while read server; do
+    echo "Server: ${server} "
+    aaflag=$(dig  @${server} ${1} soa +norec | grep ";; flags" | cut -d " " -f 4 | cut -b 1-2)
+    if [ "${aaflag}" == "aa" ]
+    then
+	echo " AA-Flag found, good "
+    else
+	echo " no AA-Flag, Server not authoritative "
+    fi
+done
