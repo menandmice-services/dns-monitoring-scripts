cc511a86459785fdff632da9426d9bfa18b71bed
Carsten Strotmann <carsten@strotmann.de>
2016-11-02 16:28:09 +0100

    Add comment to Test 11

--- test11.sh
+++ test11.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Test 11 - test for DNSKEY RRset answer size. The full answer packet
+# of the DNSKEY rrset should be below the IPv6 fragmentation payload
+# limit (1232 byte)
 maxsize=1232
 replysize=$(dig ${1} dnskey +dnssec | grep ";; MSG SIZE" | cut -d " " -f 6)
 if [ "${replysize}" -le "${maxsize}" ]
