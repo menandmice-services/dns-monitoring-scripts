7edba01435b6a1b347d0df11905197e0f21b65c4
Carsten Strotmann <carsten@strotmann.de>
2016-11-02 23:00:52 +0100

    Changed persistent storage filenames

--- test15.sh
+++ test15.sh
@@ -2,10 +2,10 @@
 # Test 15 Count of DNSKEY records in the zone. The number can change
 # during a key-rollover. Any change should create an WARNING event
 
-olddnskeycount=$(cat $0.saved.dnskeycount)
+olddnskeycount=$(cat $0.$1.saved.dnskeycount)
 
 dnskeycount=$(dig ${1} dnskey +dnssec | egrep "DNSKEY.*2" | grep -v "RRSIG" | wc -l)
-echo "${dnskeycount}" > $0.saved.dnskeycount
+echo "${dnskeycount}" > $0.$1.saved.dnskeycount
 
 if [ "${dnskeycount}" != "${olddnskeycount}" ]
 then
