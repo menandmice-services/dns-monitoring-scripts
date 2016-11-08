546ccd1ff13d55900654658cda9c6f015778d3f2
Carsten Strotmann <carsten@strotmann.de>
2016-11-08 23:56:54 +0100

    some more Linux fixes

--- test01.sh
+++ test01.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-# Test 1 - UDPv4 reachability - test for each authoritative server of the DNS infrastructure
-dig -4 +nssearch ${1}
+# Test 1 - UDPv4 reachability - test for each authoritative server of
+# the DNS infrastructure
+dig ns ${1} +short | while read server; do
+   ipaddr=$(dig ${server} a +short)
+   echo "Server: ${server} (${ipaddr})"
+   soarec=$(dig -4  @${server} ${1} soa +cd)
+   rc=$?
+   if [ $rc != 0 ];
+   then
+       echo "Error while sending UDPv4 query to ${server}"
+       exit $rc;
+   else
+       echo "OK"
+   fi
+done
+
