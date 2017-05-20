52d99ad6e27645d3dbec417a42d120b4cd5982de
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-20 17:15:05 +0200

    run.sh: enhanced by checking for dig and delv

--- run.sh
+++ run.sh
@@ -4,6 +4,16 @@
 # simple script to call all scripts
 #
 
+if ! type "dig" >/dev/null; then
+  echo "Make sure dig is in \$PATH. Unable to find it. Exiting..."
+  exit 1
+fi
+
+if ! type "delv" >/dev/null; then
+  echo "Make sure delv is in \$PATH. Unable to find it. Exiting..."
+  exit 1
+fi
+
 for script in test*.sh; do
   ./${script} ${1}
 done
