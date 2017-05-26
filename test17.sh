1cc7607042dd087b32e78fd9d6448c0133ebd8ba
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 23:12:16 +0200

    test17: every script should exit with a return code/value of 1 if something is not as expected *imo*

--- test17.sh
+++ test17.sh
@@ -9,12 +9,17 @@
 
 echo " == #17 - Recursion check == "
 
+err=0
+
 dig NS ${1} +short | while read server; do
   echo "Server: ${server} "
 
   if dig ${1} SOA @${server} | awk '/;; flags:/' | grep -q " ra"; then
+    err=1
     echo " Recursion enabled, bad if this is an authoritative server "
   else
     echo " Recursion disabled, good "
   fi
 done
+
+exit ${err}
