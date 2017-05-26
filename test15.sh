45787a5b6e4f7185df50aaa535c998d940cdc3a5
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:26:46 +0200

    test15: cosmetics

--- test15.sh
+++ test15.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+#
 # Test 15 Count of DNSKEY records in the zone. The number can change
 # during a key-rollover. Any change should create an WARNING event
+#
+
 echo " == #15 - DNSKEY record count == "
 
 if [ -f "$0.$1.saved.dnskeycount" ]; then
@@ -9,13 +13,11 @@ else
   echo "First run. This result won't be meaningful until the next run.";
 fi
 
-dnskeycount=$(dig ${1} dnskey +cd +dnssec | egrep "DNSKEY.*2" | grep -v "RRSIG" | wc -l)
+dnskeycount=$(dig ${1} DNSKEY +cd +dnssec | egrep "DNSKEY.*2" | grep -v "RRSIG" | wc -l)
 echo "${dnskeycount}" > $0.$1.saved.dnskeycount
 
-if [ "${dnskeycount}" != "${olddnskeycount}" ]
-then
-    echo "Warning: Number of DNSKEY-Record has changed!"
+if [ "${dnskeycount}" != "${olddnskeycount}" ]; then
+  echo "Warning: Number of DNSKEY-Record has changed!"
 else
-    echo "OK: Number of DNSKEY-Record is the same as with last test!"
+  echo "OK: Number of DNSKEY-Record is the same as with last test!"
 fi
-
