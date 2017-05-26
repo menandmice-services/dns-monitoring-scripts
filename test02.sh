dc7eb2a8e79fbf86a8552bf251d7474271ec78ad
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:07:27 +0200

    test02: cosmetics

--- test02.sh
+++ test02.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
+
+#
 # Test 2 - UDPv6 reachability - test for each authoritative server of
 # the DNS infrastructure
+#
+
 echo " == #2 - UDPv6 reachability == "
-dig ns ${1} +short | while read server; do
-   ipaddr=$(dig ${server} aaaa +short)
-   echo "Server: ${server} (${ipaddr})"
-   soarec=$(dig -6  @${server} ${1} soa +cd)
-   rc=$?
-   if [ $rc != 0 ];
-   then
-       echo "Error while sending UDPv6 query to ${server}"
-       exit $rc;
-   else
-       echo "OK"
-   fi
+
+dig NS ${1} +short | while read server; do
+  ipaddr=$(dig ${server} AAAA +short)
+
+  echo "Server: ${server} (${ipaddr})"
+  soarec=$(dig -6 @${server} ${1} SOA +cd)
+  rc=$?
+
+  if [ ${rc} != 0 ]; then
+    echo "Error while sending UDPv6 query to ${server}"
+    exit ${rc};
+  else
+    echo "OK"
+  fi
 done
 
