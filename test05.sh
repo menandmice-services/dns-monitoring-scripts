3fa4674ce4bd0f68de585ea8deff80c4d8631b89
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 23:02:58 +0200

    test05: every script should exit with a return code/value of 1 if something is not as expected *imo*

--- test05.sh
+++ test05.sh
@@ -8,7 +8,9 @@
 
 echo " == #5 - EDNS0 response size == "
 
+err=0
 ednspolicy=1232
+
 dig NS ${1} +short | while read server; do
   echo "Server: ${server} "
   ednsbuf=$(dig @${server} ${1} | grep "; EDNS:" | cut -d " " -f 7)
@@ -16,6 +18,9 @@ dig NS ${1} +short | while read server; do
   if [ "${ednsbuf}" -eq "${ednspolicy}" ]; then
     echo " EDNS0-Bufsize is ${ednsbuf}, good "
   else
+    err=1
     echo " EDNS0-Bufsize is ${ednsbuf}, out of policy range "
   fi
 done
+
+exit ${err}
