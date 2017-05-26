0c520f861d26dec9404e9bad0b209b26ea8f89e2
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:05:50 +0200

    test01: cosmetics

--- test01.sh
+++ test01.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
+
+#
 # Test 1 - UDPv4 reachability - test for each authoritative server of
 # the DNS infrastructure
+#
+
 echo " == #1 - UDPv4 reachability == "
-dig ns ${1} +short | while read server; do
-   ipaddr=$(dig ${server} a +short)
-   echo "Server: ${server} (${ipaddr})"
-   soarec=$(dig -4  @${server} ${1} soa +cd)
-   rc=$?
-   if [ $rc != 0 ];
-   then
-       echo "Error while sending UDPv4 query to ${server}"
-       exit $rc;
-   else
-       echo "OK"
-   fi
+
+dig NS ${1} +short | while read server; do
+  ipaddr=$(dig ${server} A +short)
+
+  echo "Server: ${server} (${ipaddr})"
+  soarec=$(dig -4 @${server} ${1} SOA +cd)
+  rc=$?
+
+  if [ ${rc} != 0 ]; then
+    echo "Error while sending UDPv4 query to ${server}"
+    exit ${rc};
+  else
+    echo "OK"
+  fi
 done
 
