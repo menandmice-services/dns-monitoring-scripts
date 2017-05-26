c0fb02a671834155e0a0bd2250dd664462771b1f
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 23:18:28 +0200

    run.sh: validate the return value of all scripts and display failed scripts

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@
 # simple script to call all scripts
 #
 
+err=0
+failed=""
+
 if ! type "dig" >/dev/null; then
   echo "Make sure dig is in \$PATH. Unable to find it. Exiting..."
   exit 1
@@ -16,4 +19,15 @@ fi
 
 for script in test*.sh; do
   sh ./${script} ${1}
+  rc=$?
+
+  if [ ${rc} != 0 ]; then
+    err=1
+    failed="${failed} ${script}"
+  fi
 done
+
+if [ "$err" != 0 ]; then
+  echo "The following scripts failed: ${failed}..."
+  exit ${err}
+fi
