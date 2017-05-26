1da3e3dd08f40c2315bfc936045a24410089a1f9
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:22:08 +0200

    test11: cosmetics

--- test11.sh
+++ test11.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+
+#
 # Test 11 - test for DNSKEY RRset answer size. The full answer packet
 # of the DNSKEY rrset should be below the IPv6 fragmentation payload
 # limit (1232 byte)
+#
+
 echo " == #11 - test for DNSKEY RRset answer size == "
 
 maxsize=1232
-replysize=$(dig ${1} dnskey +dnssec +cd | grep ";; MSG SIZE" | cut -d " " -f 6)
-if [ "${replysize}" -le "${maxsize}" ]
-then
-    echo "Good, DNSKEY RRSet size is ${replysize} which is below or equal to ${maxsize}"
+replysize=$(dig ${1} DNSKEY +dnssec +cd | grep ";; MSG SIZE" | cut -d " " -f 6)
+
+if [ "${replysize}" -le "${maxsize}" ]; then
+  echo "Good, DNSKEY RRSet size is ${replysize} which is below or equal to ${maxsize}"
 else
-    echo "Bad, DNSKEY RRSet size is ${replysize} which is above ${maxsize}"
+  echo "Bad, DNSKEY RRSet size is ${replysize} which is above ${maxsize}"
 fi
