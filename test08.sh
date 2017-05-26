91ce6ec7025b6296a155185512327a05f95f95f1
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:17:57 +0200

    test08: cosmetics

--- test08.sh
+++ test08.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+#
 # test that all authoritative server for a zone have the same SOA
 # serial. The return code of the dig command should be checked for
 # errors.  the SOA serial can be different for short amount of times
@@ -7,20 +9,21 @@
 # warning if the same SOA difference is seen in two successive tests
 # is the same SOA difference seen after three or more tests, an event
 # of severity ERROR should be generated
+#
+
 echo " == #8 - SOA serial == "
 
 oldsoaserial="0"
 dig ${1} +nssearch | while read serverres; do
-    soaserial=$(echo ${serverres} | cut -d ' ' -f 4)
-    if [ "${oldsoaserial}" -eq "0" ]
-    then
-	oldsoaserial=$soaserial
+  soaserial=$(echo ${serverres} | cut -d ' ' -f 4)
+
+  if [ "${oldsoaserial}" -eq "0" ]; then
+    oldsoaserial=${soaserial}
+  else
+    if [ "${oldsoaserial}" -eq "${soaserial}" ]; then
+      echo "Match for ${soaserial}"
     else
-	if [ "${oldsoaserial}" -eq "${soaserial}" ]
-	then
-	    echo "Match for ${soaserial}"
-	else
-	    echo "Mismatch for ${soaserial} != ${oldsoaserial}"
-	fi
+      echo "Mismatch for ${soaserial} != ${oldsoaserial}"
     fi
+  fi
 done
