bd893084e7600bb6002dc533578c8ac9c3d846e4
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:25:51 +0200

    test14: cosmetics

--- test14.sh
+++ test14.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+#
 # Test 14 - DS Records and KSK - test that the DS-Record matches the
 # KSK in the zone. The two values (Key-ID) must match.
+#
+
 echo " == #14 - DS Records and KSK == "
 
 if [ "$1" = "" ]; then
@@ -8,13 +12,11 @@ if [ "$1" = "" ]; then
   exit 1
 fi
 
-dskeyid=$(dig ${1} ds +short +cd | cut -d " " -f 1 | tail -1)
-rrsigkeyid=$(dig ${1} dnskey +dnssec +short +cd | egrep "^DNSKEY" | grep "${dskeyid}" | cut -d ' ' -f 7)
+dskeyid=$(dig ${1} DS +short +cd | cut -d " " -f 1 | tail -1)
+rrsigkeyid=$(dig ${1} DNSKEY +dnssec +short +cd | egrep "^DNSKEY" | grep "${dskeyid}" | cut -d ' ' -f 7)
 
-if [ "${dskeyid}" != "${rrsigkeyid}" ]
-then
-    echo "Error: Key-Tag of DS-Records does not match the Key-Tag of RRSIG on DNSKEY"
+if [ "${dskeyid}" != "${rrsigkeyid}" ]; then
+  echo "Error: Key-Tag of DS-Records does not match the Key-Tag of RRSIG on DNSKEY"
 else
-    echo "OK: Key-Tag of DS-Records does match the Key-Tag of RRSIG on DNSKEY"
+  echo "OK: Key-Tag of DS-Records does match the Key-Tag of RRSIG on DNSKEY"
 fi
-  
