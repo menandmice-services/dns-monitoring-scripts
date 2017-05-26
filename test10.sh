36df7165cc02c1a987075f94a0090a863b93472f
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:21:10 +0200

    test10: cosmetics

--- test10.sh
+++ test10.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+#
 # Test 10 - test for Parent-Child NS-RRset. Tests that the NS-RRset in
 # the parent zone (delegation) matches the NS-RRset in the zone data.
+#
+
 echo " == #10 - test for Parent-Child NS-RRset == "
 
 if [ "$1" = "" ]; then
@@ -9,22 +13,23 @@ if [ "$1" = "" ]; then
 fi
 
 # get one authoritative server for the zone
-child_dns=$(dig ns ${1} +short | tail -1)
+child_dns=$(dig NS ${1} +short | tail -1)
 # get TLD of Domain
 tld=$(echo ${1} | rev | cut -d'.' -f 1 | rev)
 # get one authoritative server for the TLD
-tldns=$(dig ns ${tld}. +short | tail -1)
+tldns=$(dig NS ${tld}. +short | tail -1)
 # query the delegation records
-parns=$(dig @${tldns} ns ${1}  +norec +noall +authority | grep "IN.*NS" | sort)
+parns=$(dig @${tldns} NS ${1} +norec +noall +authority | grep "IN.*NS" | sort)
+
 while read nsrec; do
-    ns=$(echo ${nsrec} | cut -d ' ' -f 5)
-    parentns="${parentns} ${ns}"
+  ns=$(echo ${nsrec} | cut -d ' ' -f 5)
+  parentns="${parentns} ${ns}"
 done <<EOF
 ${parns}
 EOF
 
 # query the zone records
-childns=$(dig @${child_dns} ns ${1} +short +norec | sort)
+childns=$(dig @${child_dns} NS ${1} +short +norec | sort)
 parentns=$(echo ${parentns} | tr ' ' '\n' | sort)
 
 echo "Parent delegation:"
@@ -33,7 +38,7 @@ echo "Child zonedata:"
 echo ${childns}
 
 if [ "${childns}" = "${parentns}" ]; then
-    echo "Parent/Child NS-RRSet matches"
+  echo "Parent/Child NS-RRSet matches"
 else
-    echo "Parent/Child NS-RRSet mismatch"
+  echo "Parent/Child NS-RRSet mismatch"
 fi
