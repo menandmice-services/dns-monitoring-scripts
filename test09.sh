96f36b9d64255c0fd5f21535e94b4a5c658f6bc0
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:19:07 +0200

    test09: cosmetics

--- test09.sh
+++ test09.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
+
+#
 # Test 9 - test for AA-Flag. Repeat this test for each authoritative
 # server for the zone. Each server must respond with an AA-Flag
+#
+
 echo " == #9 - AA-Flag == "
 
-dig ns ${1} +short | while read server; do
-    echo "Server: ${server} "
-    aaflag=$(dig  @${server} ${1} soa +norec | grep ";; flags" | cut -d " " -f 4 | cut -b 1-2)
-    if [ "${aaflag}" = "aa" ]
-    then
-	echo " AA-Flag found, good "
-    else
-	echo " no AA-Flag, Server not authoritative "
-    fi
+dig NS ${1} +short | while read server; do
+  echo "Server: ${server} "
+  aaflag=$(dig @${server} ${1} SOA +norec | grep ";; flags" | cut -d " " -f 4 | cut -b 1-2)
+
+  if [ "${aaflag}" = "aa" ]; then
+    echo " AA-Flag found, good "
+  else
+    echo " no AA-Flag, Server not authoritative "
+  fi
 done
