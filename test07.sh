2fcce61afa674f9f5c43e8a184ef74a76cec7d74
Jean-Michel Brünn <jean.bruenn@ip-minds.de>
2017-05-26 22:16:16 +0200

    test07: cosmetics

--- test07.sh
+++ test07.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
+
+#
 # Test 7 - test that all authoritative server for a zone respond on
 # TCP/IPv4. Count is tested against the number of delegation
 # authoritative servers for the zone.
+#
+
 echo " == #7 - IPv4 (TCP) zone response tests == "
 
 # get TLD for the zone
 tld=$(echo ${1} | rev | cut -d'.' -f 1 | rev)
 # pick one TLD auth server
-tldns=$(dig ns ${1}. +short | tail -1)
+tldns=$(dig NS ${1}. +short | tail -1)
 # query and count the delegation NS records for the zone
-parentnsnum=$(dig @${tldns} ns ${1} +short | wc -l)
+parentnsnum=$(dig @${tldns} NS ${1} +short | wc -l)
 # query the authoritative DNS servers for the zone
 childnsnum=$(dig -4 ${1} +nssearch +tcp | wc -l)
 
-if [ "${parentnsnum}" -eq "${childnsnum}" ]
-then
-   echo "all authoritative DNS-Server answer"
+if [ "${parentnsnum}" -eq "${childnsnum}" ]; then
+  echo "all authoritative DNS-Server answer"
 else
-    echo "Error: Mismatch"
-    echo "Auth DNS-Servers in Delegation: ${parentnsnum}"
-    echo "Auth DNS-Servers in answering: ${childnsnum}"
+  echo "Error: Mismatch"
+  echo "Auth DNS-Servers in Delegation: ${parentnsnum}"
+  echo "Auth DNS-Servers in answering: ${childnsnum}"
 fi
-    
